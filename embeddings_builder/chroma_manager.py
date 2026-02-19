dd2b04e3a8c3803b1710a63487a0456d3e9a341a
NGanzyuk <nganzyuk.edu@gmail.com>
2026-02-19 10:24:01 +0300

    add new features

---
+++ embeddings_builder/chroma_manager.py
@@ -0,0 +1,42 @@
+from typing import List, Dict, Any
+
+import chromadb
+
+
+def save_to_chroma_collection(
+        client: chromadb.PersistentClient,
+        collection_name: str,
+        ids: List[str],
+        embeddings: List[List[float]],
+        metadatas: List[Dict[str, Any]],
+        documents: List[str],
+):
+    collection = client.get_or_create_collection(name=collection_name)
+    collection.upsert(
+        ids=ids,
+        embeddings=embeddings,
+        metadatas=metadatas,
+        documents=documents,
+    )
+
+
+def query_chroma_collection(
+        collection: chromadb.Collection,
+        query_embedding: List[float],
+        top_k: int = 5,
+) -> List[Dict[str, Any]]:
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=top_k,
+        include=["documents", "metadatas", "distances"],
+    )
+    formatted = []
+    for i in range(len(results["documents"][0])):
+        formatted.append(
+            {
+                "document": results["documents"][0][i],
+                "metadata": results["metadatas"][0][i],
+                "distance": results["distances"][0][i],
+            }
+        )
+    return formatted
---
+++ main.py
@@ -0,0 +1,10 @@
+from embedding_analyzer.visualization import analyze_embeddings
+from download_and_build_corpus import build_and_save_corpus
+from embeddings_builder import build_embeddings
+
+
+
+if __name__ == "__main__":
+    build_and_save_corpus()
+    #build_embeddings()
+    #analyze_embeddings()
