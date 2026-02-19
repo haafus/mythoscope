5a7f00c5e8996e5e8d829ed1fd53267c38aabb31
NGanzyuk <nganzyuk.edu@gmail.com>
2026-02-19 10:04:37 +0300

    add new features

---
+++ embeddings_builder/config.py
@@ -0,0 +1,9 @@
+DEFAULTS = {
+    "corpus_dir": "corpus",
+    "out_dir": "analysis",
+    "chroma_path": "./chroma_db",
+    "cache_dir": "./cache",
+    "embedding_model": "BAAI/bge-m3",
+    "chunking": "paragraph_based",
+    "text_type": "translate",
+}
